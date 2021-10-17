08fa7f14ca540e5df7727fa8e6ec7764740fbca9
Wman1001 <wichert.wessels@live.nl>
2021-10-17 17:15:00 +0200

    cnn working

--- NLP_Code/preprocess/testsomething.py
+++ NLP_Code/preprocess/testsomething.py
@@ -3,7 +3,5 @@ from gensim import models
 
 original_word2vec = r'C:\Users\20174868\Desktop\NLP_Code\word2vec_dutch\model.bin'
 model = models.KeyedVectors.load_word2vec_format(original_word2vec, binary=True)
-
 word2vec_model = KeyedVectors.load(r"C:\Users\20174868\Desktop\NLP_Code\preprocess\word2vec_dutch_pickle.p")
-print(word2vec_model["man"])
-# print(word2vec_model.most_similar("man"))
+
